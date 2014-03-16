7cafc5788b52c25268b18162c67aa0d498d82393
trevor-umeda <FroggyP@gmail.com>
2014-03-16 16:48:28 -0700

    adding tagging

--- grails-app/controllers/com/imageCollect/TagController.groovy
+++ grails-app/controllers/com/imageCollect/TagController.groovy
@@ -3,9 +3,16 @@ package com.imageCollect
 class TagController {
 
     def tagImage() {
-        def tag = Tag.findOrCreateByName(request.JSON?.tag)
-        def image = Image.findById(request.JSON?.id)
-
+        def tag
+        def image
+        if (params.tag){
+            tag = Tag.findOrCreateByName(params.tag)
+            image = Image.findById(params.id)
+        }
+        else{
+            tag = Tag.findOrCreateByName(request.JSON?.tag)
+            image = Image.findById(request.JSON?.id)
+        }
         image.addToTags(tag)
         image.save()
 
