d034d0f4010cd14aa30dd2aebc812e8b46118827
Trevor Umeda <tumeda@riotgames.com>
2017-03-31 23:00:56 -0700

    trying to change url mapping to make sense

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -25,7 +25,7 @@ class UrlMappings {
             action = [ GET:"getTags", POST:"tagImage" ]
         }
 
-        "/tagImage"(controller: "tagImage") {
+        "/tagImage/tagImage"(controller: "tagImage") {
             action = [ POST:"tagImage" ]
         }
 
